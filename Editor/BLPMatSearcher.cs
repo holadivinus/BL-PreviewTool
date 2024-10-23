b8b61437f0dee1fd4a843426667142252abf5d11
holadivinus <holadivinus@gmail.com>
2024-10-23 10:29:55 -0700

    LateLoad fix

--- Editor/BLPMatSearcher.cs
+++ Editor/BLPMatSearcher.cs
@@ -179,6 +179,7 @@ namespace BLPTool
                             }
                         }
                         e = (MaterialEditor)MaterialEditor.CreateEditor(mat);
+                        found = mat;
                     }
                     if (found == null)
                     {
