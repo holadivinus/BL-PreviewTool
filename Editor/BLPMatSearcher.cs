0b8f2793b77fa59a7733a3e443f38dbbfbba22f5
holadivinus <holadivinus@gmail.com>
2024-10-24 11:05:05 -0700

    Fixed stupid search compilation error

--- Editor/BLPMatSearcher.cs
+++ Editor/BLPMatSearcher.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEditor;
-using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.UIElements;
